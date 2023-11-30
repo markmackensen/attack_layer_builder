72dfb96646b41587e11e1dc1415f864fd8063bfb
markmackensen <149718331+markmackensen@users.noreply.github.com>
2023-11-29 17:03:36 -0800

    Update attack_layer_builder.py
    This change gives users the option to either manually input the technique IDs or select a CSV file containing the IDs. It also properly utilizes a single Tkinter root instance for both opening and saving files. Additionally, correctly placed the root.destroy() call to properly close the Tkinter window after use, for resource management. Finally, included a prompt for the user to input the version number of the ATT&CK Navigator they are using. A while loop is used to continuously prompt the user until a valid input (a number between 4 and 14) is entered.

--- attack_layer_builder.py
+++ attack_layer_builder.py
@@ -1,6 +1,8 @@
 import json  # Import the json module
 import tkinter as tk
 from tkinter import filedialog
+import csv  # Import csv for handling CSV files
+
 
 def generate_json_structure(technique_ids, score):
     techniques = []
@@ -18,14 +20,30 @@ def generate_json_structure(technique_ids, score):
         techniques.append(technique)
 
     return techniques
+    
+def get_technique_ids_from_csv(file_path):
+    with open(file_path, newline='', encoding='utf-8') as csvfile:
+        reader = csv.reader(csvfile)
+        return [row[0] for row in reader]  # Assumes Technique IDs are in the first column
 
-# Provided JSON structure
+# Prompt for ATT&CK Navigator version
+while True:
+    try:
+        attack_version_input = int(input("What version of ATT&CK Navigator are you using? (Enter a number between 4 and 14): "))
+        if 4 <= attack_version_input <= 14:
+            break
+        else:
+            print("Please enter a number between 4 and 14.")
+    except ValueError:
+        print("Invalid input. Please enter a valid number.")
+
+# Provided JSON structure with dynamic ATT&CK version
 base_structure = {
     "name": "Custom ATT&CK Layer",
     "versions": {
-        "attack": "11", # Target version of MITRE ATT&CK - Update if using a different version
-        "navigator": "4.8.0",
-        "layer": "4.4"
+        "attack": str(attack_version_input),  # Dynamic version based on user input
+        "navigator": "4.9.1",
+        "layer": "4.5"
     },
     "domain": "enterprise-attack",
     "description": "Custom Layer for User Provided TTPs",
@@ -41,8 +59,25 @@ base_structure = {
     "techniques": []  # Empty techniques list, to be filled later
 }
 
-# Prompt the user for technique IDs
-technique_ids_input = input("Enter technique IDs separated by commas: ").split(',')
+root = tk.Tk()
+root.withdraw()  # Create a single Tkinter root instance and hide the main window
+
+# User input for technique IDs or CSV file selection
+input_method = input("Enter '1' to select a CSV file or '2' to input technique IDs manually: ")
+
+if input_method == '1':
+    # GUI to select CSV file
+    file_path = filedialog.askopenfilename(filetypes=[("CSV files", "*.csv"), ("All files", "*.*")])
+    if file_path:
+        technique_ids_input = get_technique_ids_from_csv(file_path)
+    else:
+        print("No file selected, exiting.")
+        exit()
+elif input_method == '2':
+    technique_ids_input = input("Enter technique IDs separated by commas: ").split(',')
+else:
+    print("Invalid input, exiting.")
+    exit()
 
 # Prompt for score and ensure it's a valid integer
 try:
@@ -59,15 +94,13 @@ json_result = json.dumps(base_structure, indent=4)  # Makes the output more read
 print(json_result)
 
 # GUI "Save As" dialog to save the result to a .json file
-root = tk.Tk()
-root.withdraw()  # Hide the main tkinter window
-
-# Open "Save As" dialog with .json as the default extension
 file_path = filedialog.asksaveasfilename(defaultextension=".json", filetypes=[("JSON files", "*.json"), ("All files", "*.*")])
 
-# If a file location is selected, save the data
+# Save the data if a file location is selected
 if file_path:
     with open(file_path, 'w') as file:
         file.write(json_result)
+
+root.destroy()  # Properly close the Tkinter window
 
 input("Press any key to continue...")
